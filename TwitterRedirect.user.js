091794920b18e921021a30993e86c8d7b2037f89
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2022-01-07 19:39:12 +0530

    Fix issue with Bromite
    Using raw URLs instead of regex, which doesn't seem to work with Bromite's implementation of userscripts

--- TwitterRedirect.user.js
+++ TwitterRedirect.user.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name         Twitter Redirect
 // @namespace    http://nikhilmwarrier.github.io/
-// @version      0.1
+// @version      0.2
 // @description  Redirect Twitter links to Nitter (nitter.net) links
 // @author       nikhilmwarrier
-// @match        http*://*.twitter.com/*
+// @match        http://twitter.com/*
+// @match        http://www.twitter.com/*
+// @match        http://mobile.twitter.com/*
+// @match        https://twitter.com/*
+// @match        https://www.twitter.com/*
+// @match        https://mobile.twitter.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
